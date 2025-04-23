04c1bb66a465519ad20c61945d80fcce324668d0
Lazyihui <chengbaozi6@qq.com>
2025-04-23 22:06:19 +0800

    <feature>add:基础的断开 连接写完

--- Assets/Client/Src_Runtime/ClientMain.cs
+++ Assets/Client/Src_Runtime/ClientMain.cs
@@ -13,12 +13,50 @@ namespace GameClient {
         bool isTearDown;
 
         void Start() {
-            Debug.Log("ClientMain Start");
+
+            int port = 7777; // 服务器端口 一般要高于1000
+            int messageSize = 1024; // 消息大小
+            string ip = "127.0.0.1"; // 服务器IP地址
+
+            // 1. 创建客户端
+            client = new Client(messageSize);
+            client.Connect(ip, port); // 连接服务器
+
+            client.OnConnected += () => {
+                Debug.Log("链接成功");
+            };
+
+            client.OnData += (message) => {
+                Debug.Log("收到消息: " + message.ToString());
+            };
+
+            client.OnDisconnected += () => {
+                Debug.Log("链接断开");
+            };
+
+            Application.runInBackground = true; // 允许后台运行
 
         }
 
         void Update() {
 
+            if (client != null) {
+                client.Tick(10); // 处理网络消息10ms
+            }
+
+        }
+
+        void OnApplicationQuit() {
+            TearDown();
+        }
+
+        void TearDown() {
+            if (isTearDown) return;
+            isTearDown = true;
+
+            if (client != null) {
+                client.Disconnect();
+            }
         }
     }
 }
--- Assets/Server/Src_Runtime/ServerMain.cs
+++ Assets/Server/Src_Runtime/ServerMain.cs
@@ -2,18 +2,64 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using Telepathy;
 
 // 服务器主类
 
 namespace GameServer {
+
     // 服务器主类
     public class ServerMain : MonoBehaviour {
+        // 服务器主类
+        Server server; // 服务器对象
+        bool isTearDown; // 是否已经关闭
+
         void Start() {
-            Debug.Log("ServerMain Start");
+            int port = 7777; // 服务器端口 一般要高于1000
+            int messageSize = 1024; // 消息大小
+
+
+            // 1. 创建服务器
+            server = new Server(messageSize);
+            server.Start(port); // 启动服务器
+
+            server.OnConnected += (connectionId, str) => {
+                Debug.Log("链接成功: " + connectionId + " " + str);
+            };
+
+            server.OnData += (connectionId, message) => {
+                Debug.Log("收到消息: " + message.ToString() + " from " + connectionId);
+                server.Send(connectionId, message); // 回发消息
+            };
+
+            server.OnDisconnected += (connectionId) => {
+                Debug.Log("链接断开: " + connectionId);
+            };
+
+            Application.runInBackground = true; // 允许后台运行
         }
 
         void Update() {
+            if (server != null) {
+                server.Tick(10); // 处理网络消息10ms
+            }
+        }
 
+        void OnDestroy() {
+            TearDown();
+        }
+
+        void OnApplicationQuit() {
+            TearDown();
+        }
+
+        void TearDown() {
+            if (isTearDown) return;
+            isTearDown = true;
+
+            if (server != null) {
+                server.Stop();
+            }
         }
     }
 }
